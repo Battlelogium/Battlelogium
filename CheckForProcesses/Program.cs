88adbd65a4893cef3119a7e51f39cce6a60cf8cf
Ronny Chan <ronny6993@gmail.com>
2013-08-26 23:04:12 -0400

    Forgot to enable direct to campaign if offline

---
+++ CheckForProcesses/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace CheckForProcesses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
