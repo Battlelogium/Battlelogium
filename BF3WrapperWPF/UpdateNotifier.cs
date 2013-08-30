398b47a6de83f026d7a53354d2cf47184b40dc66
Ronny Chan <ronny6993@gmail.com>
2013-08-29 22:55:17 -0400

    Wrap updateChecker in a using statement

--- BF3WrapperWPF/UpdateNotifier.cs
+++ BF3WrapperWPF/UpdateNotifier.cs
@@ -21,46 +21,51 @@ namespace Battlelogium
     public class UpdateNotifier
     {
         private Version currentVersion;
-        private BackgroundWorker updateWorker;
         private Dispatcher dispatcher;
 
-        public UpdateNotifier(Version currentVersion, Dispatcher dispatcher)
+        public UpdateNotifier(Version currentVersion, Dispatcher dispatcher) 
         {
             this.currentVersion = currentVersion;
             this.dispatcher = dispatcher;
-            this.updateWorker = new BackgroundWorker();
-            this.updateWorker.DoWork += delegate { checkUpdates(); };
         }
 
         private void checkUpdates()
         {
-            Utilities.Log("Checking for new version"); 
-            WebClient updateChecker = new WebClient();
-            try
+            Utilities.Log("Checking for new version");
+            using (WebClient updateChecker = new WebClient())
             {
-                Version newVersion = new Version(updateChecker.DownloadString(new Uri("https://raw.github.com/ron975/Battlelogium/master/currentreleaseversion")));
-                Utilities.Log("Downloaded version number");
-                if (newVersion > currentVersion)
+
+                try
                 {
-                    Utilities.Log(String.Format("currentVersion = {0}, newVersion = {1}",currentVersion.ToString(), newVersion.ToString()));
-                    dispatcher.BeginInvoke(new Action(delegate{
-                        if (Utilities.ShowChoiceDialog("There is a new version available. Open the thread on the Steam Users' Forums? You can disable version checking in config.ini", "New Version Available", "Open the thread", "Ignore"))
+                    Version newVersion = new Version(updateChecker.DownloadString(new Uri("https://raw.github.com/ron975/Battlelogium/master/currentreleaseversion")));
+                    Utilities.Log("Downloaded version number");
+                    if (newVersion > currentVersion)
+                    {
+                        Utilities.Log(String.Format("currentVersion = {0}, newVersion = {1}", currentVersion.ToString(), newVersion.ToString()));
+                        dispatcher.BeginInvoke(new Action(delegate
                         {
-                            Process.Start("http://forums.steampowered.com/forums/showthread.php?t=3140249");
-                        }
-                    }));
+                            if (Utilities.ShowChoiceDialog("There is a new version available. Open the thread on the Steam Users' Forums? You can disable version checking in config.ini", "New Version Available", "Open Forum Thread", "Ignore Update"))
+                            {
+                                Process.Start("http://forums.steampowered.com/forums/showthread.php?t=3140249");
+                            }
+                        }));
+                    }
                 }
-            }
-            catch
-            {
-                Utilities.Log("Unable to get new version");
-                return;
+                catch
+                {
+                    Utilities.Log("Unable to get new version");
+                    return;
+                }
             }
         }
 
         public void run()
         {
-            this.updateWorker.RunWorkerAsync();
+            using (var updateWorker = new BackgroundWorker())
+            {
+                updateWorker.DoWork += delegate { checkUpdates(); };
+                updateWorker.RunWorkerAsync();
+            }
         }
     }
 }
