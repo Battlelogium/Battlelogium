a4c11e45de822f80d8215a3552ddf117646671a4
Ronny Chan <ronny6993@gmail.com>
2013-10-01 18:18:47 -0400

    Move remove origin requirement to helper app

--- RemoveOriginRequirement/Program.cs
+++ RemoveOriginRequirement/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 
 namespace RemoveOriginRequirement
@@ -14,7 +15,79 @@ namespace RemoveOriginRequirement
         {
 
         }
+        public void RemoveOriginRequirement()
+        {
+            string parFile = @"KgIRPFYyKXkDLQJfVBsPIngUDiNYHSUgLkcHAwhKDXtqSRwEO1YIKzNUHykTKmsfQlYBexNTUBID
+                                PGMSWSFUASwfNh04SltZfB1iSic2EkYYGwFtMykeE3YNSkYfEghYVwcLPF8UBTVDHioVLEczV10D
+                                PCVpVBIYJEotMCR8NB81DnIMQFAZPR1aTRBHIEEYAQRFFG4bKkplbEcDKQdfSBY6MUcZV1gRMnos
+                                IlwBSFATEhdTShoJNWsBFgZaRBIVO1IGT2UJYAVTVB8FKR0mHgsCQxwkIWczb1oJLy1qYgEFI0cI
+                                KyxfBSUCIlYLSlQeKx1hbT1ZYmw2Nih0LhM/GmEsZmkvDAhlcSw4H3wlKwdXQm4VN1Zlb1oLKiRE
+                                YhoGNWMQAw0RTGBSDAkzTloEJSRPeCUPPloSEj1BECMbemEKV1QDIhFVCjcPIF8eDktmGC5DfW87
+                                TWE2DDRfSBc2ElofKyBTGDMFE3oBUEELIi1TViMYP1kUFBFtBSgZPVcwU1QYOjhqYzInFWw0NiRS
+                                EiUDPG84UVQaPihYQ1M+P1wdBDl9HiEUKkE8V0AIEQ9XUBocNR0UDwATexcCLkMfRlEsJy1TdBIe
+                                OBNMVyYLLS0fIVgKWmk8Ky9fRxYyIFISHFBjFDQRJl8/QBsuKzFaSwpEB1ofRFdtJS4kE38AQFQG
+                                EgdESwAeKW84GRFUAy0VK1oOV1A2GQh4F0E1F3I8MjpiPhUiDHYzZncjHRRpayY+AGYlKxFcASYZ
+                                I1Ycf0EHPiRXRRAJNUACKwdXQm4VN1ZlYFoEOiRYUDoOcA5RQFUHQHlceAJfFQJGCgZkFEJcYApD
+                                Q1EdNQcifwJZEwxYenQ8dAEFNEYSAzZFBCQZIBNSAxcvD2FxRR4PIxF7JxdeFTUTO2cGV1kPbnwW
+                                BjELJEcdEgNYFCwUbwBNKWUYISVDRwcjNBNMVyFjS3JCegNZFwRafktmVhwANVAFPgERTGBDfwNd
+                                GgFgBy9FUBIGPFYVMwxCBTIfbw5PYlYJKzJFYBoNOUcQGzMCexMFP0MAUUEPKgVfVwcYP0BRSkVw
+                                EiMVPEArSlIDOiBackBg
+                                ";
 
-   
+
+            string bf3Path = "";
+            try
+            {
+                bf3Path = GetBF3Path();
+            }
+            catch
+            {
+                MessageBox.Show("Battlefield 3 Not Found");
+                Environment.Exit(1);
+            }
+            string parPath = Path.Combine(bf3Path, "bf3.par");
+            string origPar = Path.Combine(bf3Path, "bf3.par.orig");
+            if (!File.Exists(origPar))
+            {
+                try
+                {
+                    File.Move(parPath, origPar);
+                    File.WriteAllBytes(parPath, Convert.FromBase64String(parFile));
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error while removing Origin requirement");
+                    Environment.Exit(1);
+                }
+            }
+
+        }
+
+        private string GetBF3Path()
+        {
+            string bf3Path;
+            try
+            {
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    bf3Path =
+                        Registry.GetValue(
+                            @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\EA Games\Battlefield 3", "Install Dir", "").ToString();
+               
+                }
+                else
+                {
+                    bf3Path =
+                        Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\EA Games\Battlefield 3", "Install Dir", "").ToString();
+                   
+                }
+                if (bf3Path == "") throw new Exception(); //throw if BF3 path not found
+                return bf3Path;
+            }
+            catch (Exception)
+            {
+                throw new FileNotFoundException("Battlefield 3 not found");
+            }
+        }
     }
 }
