4fbf302b21e02036ed7993eb95ef54e36b1aa930
Ronny Chan <ronny6993@gmail.com>
2014-02-08 18:30:53 -0500

    Remove old branch files

--- BF3WrapperWPF/customjs.js
+++
@@ -1,1 +0,0 @@
-﻿//Do not use alert();
---
+++ Battlelogium.UI/BF3Window.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Battlelogium.Core;
+using CefSharp.Wpf;
+using Battlelogium;
+namespace Battlelogium.UI.BF3
+{
+
+    public partial class BF3Window : Window
+    {
+        public BF3Window()
+        {
+            InitializeComponent();
+            Battlefield3 bf3blog = new Battlefield3(this);
+            UICore process = new UICore(bf3blog, this, this.MainGrid);
+        }
+    }
+}
