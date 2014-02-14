fd0d55dd2a545950812aadf1bf4ab47d5e75c7aa
Ronny Chan <ronny6993@gmail.com>
2014-02-14 01:07:39 -0500

    Add a class to help wait for process start

--- Battlelogium.Core/Utilities/Process.cs
+++ Battlelogium.Core/Utilities/Process.cs
@@ -7,13 +7,13 @@ using System.Management;
 using System.Runtime.InteropServices;
 using Microsoft.Win32;
 using System.Diagnostics;
+using System.ComponentModel;
 
 
 namespace Battlelogium.Core.Utilities
 {
-    class ProcessTools
+    public class ProcessTools
     {
-
         public static void CreateOrphanedProcess(string path, string args)
         {
             string commandLine = @"""" + path + @""" " + args;
@@ -66,4 +66,77 @@ namespace Battlelogium.Core.Utilities
 
 
     }
+
+    public class ProcessStartWaiter
+    {
+        public string processName;
+
+        private bool loop = true;
+        public delegate void ProcessStartEventHandler(object sender, ProcessStartEventArgs e);
+        public event ProcessStartEventHandler ProcessStart;
+       
+        // Adapted from http://stackoverflow.com/questions/6575117/
+        public ProcessStartWaiter(string processName)
+        {
+            this.processName = processName;
+        }
+
+        public void Listen()
+        {
+            var query = new WqlEventQuery(
+                "__InstanceCreationEvent",
+                new TimeSpan(0, 0, 1),
+                "TargetInstance isa \"Win32_Process\" and TargetInstance.Name = '" + processName + "'"
+              );
+
+            using (var watcher = new ManagementEventWatcher(query)){
+                while (this.loop)
+                {
+                    ManagementBaseObject wmiEvent = watcher.WaitForNextEvent();
+                    ManagementBaseObject targetInstance = (ManagementBaseObject)wmiEvent["targetInstance"];
+                    int pid = Convert.ToInt32(targetInstance["processID"]);
+                    Process process = Process.GetProcessById(pid);
+                    if(this.loop) this.OnProcessStart(new ProcessStartEventArgs(process, this.processName)); //Don't trigger event if we want to stop sending them.
+                    wmiEvent.Dispose();
+                    targetInstance.Dispose();
+                    process.Dispose();
+                }
+                watcher.Stop();
+            }
+        }
+
+        public void StopListen()
+        {
+            this.loop = false;
+        }
+
+        public void ListenAsync()
+        {
+            using (var worker = new BackgroundWorker())
+            {
+                worker.DoWork += delegate { this.Listen(); };
+                worker.RunWorkerAsync();
+            }
+        }
+
+        private void OnProcessStart(ProcessStartEventArgs e)
+        {
+            if (ProcessStart != null)
+                ProcessStart(this, e);
+        }
+    }
+
+    public class ProcessStartEventArgs : EventArgs
+    {
+        public Process Process { get; private set; }
+        public string ProcessName { get; private set; }
+
+        public ProcessStartEventArgs(Process process, string processName)
+        {
+            Process = process;
+            ProcessName = processName;
+            
+        }
+    }
+
 }
