ae0eccdbf690c38636d1cea5f601ea1b09357051
Ronny Chan <ronny6993@gmail.com>
2014-02-27 21:52:44 -0500

    Fix Overlay Assemblyinfo

--- Battlelogium.Overlay/Properties/AssemblyInfo.cs
+++ Battlelogium.Overlay/Properties/AssemblyInfo.cs
@@ -16,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ecf91f9c-4317-431f-8897-71a54576b608")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
