2e1cbbb79f5b6861ed5bc85deecfe366bf9656b1
Ronny Chan <ronny6993@gmail.com>
2014-03-10 21:34:50 -0400

    Release information

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -21,5 +21,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+// Versioning format follows Semantic Versioning, Major.Minor.Patch, with last digit ignored and left as zero.
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
