a6099c7ee50c7aa3894cc722f0e2a1ea83d94803
Ronny Chan <ronny6993@gmail.com>
2014-06-13 21:40:57 -0400

    Add backend for Hardline support

---
+++ Battlelogium.Core/Battlelog/BattlefieldHardline.cs
@@ -0,0 +1,19 @@
+﻿using Battlelogium.Core;
+using Battlelogium.Core.UI;
+namespace Battlelogium.Core.Battlelog
+{
+    public class BattlefieldHardline : BattlelogBase
+    {
+#if DEBUG
+        //static string jsUrl = "http://ron975.github.io/Battlelogium/Battlelogium.Core/Javascript/battlelog.bfh.inject.js";
+
+        static string jsUrl = "http://localhost/battlelogium/battlelog.bfh.inject.min.js";
+#else
+        static string jsUrl = "http://ron975.github.io/Battlelogium/Battlelogium.Core/Javascript/battlelog.bfh.inject.min.js";
+#endif
+        public BattlefieldHardline()
+            : base("http://battlelog.battlefield.com/bfh/", "Battlefield Hardline", "BFH", new string[1] { "bfh.exe" }, 1024944, jsUrl)
+        {
+        }
+    }
+}
