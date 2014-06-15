594db8d701e1f7c43e2f3b4390bcffc9da7bcd37
Ronny Chan <ronny6993@gmail.com>
2014-06-15 14:07:02 -0400

    Add MOHW client fixes #44

---
+++ Battlelogium.UI.MOHW/MOHWWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Battlelogium.Core.Battlelog;
+using Battlelogium.Core.UI;
+
+namespace Battlelogium.UI.MOHW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MOHWWindow : UIWindow
+    {
+        public MOHWWindow()
+        {
+            InitializeComponent();
+            this.MainControl = this.mainControl;
+            this.InitializeCore(new MedalOfHonorWarfighter());
+        }
+    }
+}
