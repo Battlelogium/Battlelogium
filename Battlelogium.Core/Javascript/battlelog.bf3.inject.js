1587c9d6f5e57ad7b3b371d775ab56d551a79e38
Ronny Chan <ronny6993@gmail.com>
2014-06-19 01:30:01 -0400

    Fix spacing in force update option

--- Battlelogium.Core/Javascript/battlelog.bf3.inject.js
+++ Battlelogium.Core/Javascript/battlelog.bf3.inject.js
@@ -3,6 +3,7 @@
 /// <reference path="dialog/battlelog.bf3.dialog.js" />
 /// <reference path="stats/battlelog.bf3.stats.js" />
 var baseurl = 'http://ron975.github.io/Battlelogium/Battlelogium.Core/Javascript';
+
 function injectOnce() {
     if (document.getElementById('_windowbutton') == null) {
         injectScript('_windowbutton', baseurl+'/windowbutton/battlelog.windowbutton.min.js');
--- Battlelogium.Core/Javascript/settings/battlelog.bf3.settings.js
+++ Battlelogium.Core/Javascript/settings/battlelog.bf3.settings.js
@@ -7,7 +7,7 @@
                     section.attr('class', 'profile-edit-row')
                     section.attr('style', 'border-bottom: 1px solid #ededed;  margin-bottom: 12px;')
                     section.attr('id', 'battlelogSettings')
-                    section.html('<div class="label"> <label>Battlelogium Settings</label> </div> <div> <p>Edit Battlelogium Settings</p><br><a onclick="app.opensettings()" href="#" style="margin-right: 16px;">Edit Program Settings</a><a onclick="app.clearcache()" href="#">Clear Cache and Cookies</a><a onclick="app.forceupdate()" href="#">Force Update</a> </div><div style="clear:both;float:none;"></div>')
+                    section.html('<div class="label"> <label>Battlelogium Settings</label> </div> <div> <p>Edit Battlelogium Settings</p><br><a onclick="app.opensettings()" href="#" style="margin-right: 16px;">Edit Program Settings</a><a onclick="app.clearcache()" href="#" style="margin-right: 16px;>Clear Cache and Cookies</a><a onclick="app.forceupdate()" href="#">Force Update</a> </div><div style="clear:both;float:none;"></div>')
                     if (!$("label:contains('Battlelogium Settings')").length) {
                         $('.profile-edit-gravatar-info').before(section);
                     }
